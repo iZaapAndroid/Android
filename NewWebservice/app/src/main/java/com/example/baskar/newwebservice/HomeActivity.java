73d51c12b6b1338dbd48428acd13c1899b4efbac
baskar <baskar>
2016-06-27 12:52:51 +0530

    add webservice example

---
+++ NewWebservice/app/src/main/java/com/example/baskar/newwebservice/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.baskar.newwebservice;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
